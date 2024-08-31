e07a7ef3e957507e1b560f00e9240242a59702c3
sadik-fattah <sadikfattah5@gmail.com>
2024-08-31 04:58:52 +0100

    sum code

--- app/src/main/java/com/guercifzone/TifnaghX/Activities/Alphabet.java
+++ app/src/main/java/com/guercifzone/TifnaghX/Activities/Alphabet.java
@@ -1,6 +1,10 @@
 package com.guercifzone.TifnaghX.Activities;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,7 +14,17 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.guercifzone.TifnaghX.R;
 
-public class Alphabet extends AppCompatActivity {
+public class Alphabet extends AppCompatActivity implements View.OnClickListener{
+    ImageButton previous, refresh, next;
+    TextView text;
+    int count;
+    MediaPlayer audio;
+    int[] id = {R.raw.a,R.raw.b,R.raw.c,R.raw.d,R.raw.e,
+            R.raw.f,R.raw.g,R.raw.h,R.raw.i,R.raw.j,
+            R.raw.k,R.raw.l,R.raw.m,R.raw.n,R.raw.o,
+            R.raw.p,R.raw.q,R.raw.r,R.raw.s, R.raw.t,
+            R.raw.u,R.raw.v,R.raw.w,R.raw.x,R.raw.y,
+            R.raw.z};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +36,21 @@ public class Alphabet extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        previous = findViewById(R.id.previous);
+        refresh = findViewById(R.id.refresh);
+        next = findViewById(R.id.next);
+        text = findViewById(R.id.letter);
+        audio = MediaPlayer.create(this, id[count]);
+        audio.start();
+
+        previous.setOnClickListener(this);
+        refresh.setOnClickListener(this);
+        next.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
--- app/src/main/java/com/guercifzone/TifnaghX/Home.java
+++ app/src/main/java/com/guercifzone/TifnaghX/Home.java
@@ -1,8 +1,10 @@
 package com.guercifzone.TifnaghX;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -16,8 +18,12 @@ import com.guercifzone.TifnaghX.Activities.Game1;
 import com.guercifzone.TifnaghX.Activities.Game2;
 import com.guercifzone.TifnaghX.Activities.Rhytem;
 
+import org.w3c.dom.Text;
+
 public class Home extends AppCompatActivity {
     Button alphabet, game1, game2, rhyme;
+    TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,46 +38,46 @@ public class Home extends AppCompatActivity {
         game1 = findViewById(R.id.game1);
         game2 = findViewById(R.id.game2);
         rhyme = findViewById(R.id.rhyme);
-
+       textView = findViewById(R.id.contactus);
         alphabet.setOnClickListener(v -> {
             if (alphabet.getId() == R.id.alphabet) {
                 Intent intent = new Intent(Home.this, Alphabet.class);
                 startActivity(intent);
             }
             else {
-                Toast.makeText(this, "This content is not available", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "ⵟⵀⵉⵙ ⵛⵓⵏⵜⵏⵜ ⵉⵙ ⵏⵓⵜ ⴰⴹⴰⵉⵍⴰⴱⵍ", Toast.LENGTH_SHORT).show();
             }
         });
-
         game1.setOnClickListener(v -> {
             if (game1.getId() == R.id.game1) {
                 Intent intent = new Intent(Home.this, Game1.class);
                 startActivity(intent);
             }
             else {
-                Toast.makeText(this, "This content is not available", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "ⵟⵀⵉⵙ ⵛⵓⵏⵜⵏⵜ ⵉⵙ ⵏⵓⵜ ⴰⴹⴰⵉⵍⴰⴱⵍ", Toast.LENGTH_SHORT).show();
             }
         });
-
         game2.setOnClickListener(v -> {
             if (game2.getId() == R.id.game2) {
                 Intent intent = new Intent(Home.this, Game2.class);
                 startActivity(intent);
             }
             else {
-                Toast.makeText(this, "This content is not available", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "ⵟⵀⵉⵙ ⵛⵓⵏⵜⵏⵜ ⵉⵙ ⵏⵓⵜ ⴰⴹⴰⵉⵍⴰⴱⵍ", Toast.LENGTH_SHORT).show();
             }
         });
-
         rhyme.setOnClickListener(v -> {
             if (rhyme.getId() == R.id.rhyme) {
                 Intent intent = new Intent(Home.this, Rhytem.class);
                 startActivity(intent);
             }
             else {
-                Toast.makeText(this, "This content is not available", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "ⵟⵀⵉⵙ ⵛⵓⵏⵜⵏⵜ ⵉⵙ ⵏⵓⵜ ⴰⴹⴰⵉⵍⴰⴱⵍ", Toast.LENGTH_SHORT).show();
             }
         });
-
+        textView.setOnClickListener(v ->{
+    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/@sadikdev"));
+    startActivity(intent);
+});
     }
 }
